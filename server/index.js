694245351c46362fe9714f35412f77c8cd17231c
ayush <ayushmehrotra1234@gmail.com>
2023-11-02 23:30:22 +0530

    Added dotenv file

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv';
 
 
 const app = express();
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb" , extended : true})  )
@@ -14,10 +16,10 @@ app.use(bodyParser.urlencoded({limit: "30mb" , extended : true}))
 app.use(cors())
 app.use('/posts' , postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://ayush:Asdfghjkl%401234@fotopia.jtm0lud.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+// const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000 ;
 
-mongoose.connect(CONNECTION_URL , {useNewUrlParser : true , useUnifiedTopology : true})
+mongoose.connect(process.env.CONNECTION_URL , {useNewUrlParser : true , useUnifiedTopology : true})
 .then(() => app.listen(PORT , () => console.log(`SERVER RUNNING ON PORT : ${PORT}`)))
 .catch((error)=> console.log(error.message))
 
