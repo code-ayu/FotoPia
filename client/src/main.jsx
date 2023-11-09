5a1e791894f711e46584d0da883530530760fc9e
ayush <ayushmehrotra1234@gmail.com>
2023-11-09 23:49:37 +0530

    build error fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ import reducers from './reducers'
 import './index.css'
 
 
-const store = configureStore(reducers , compose(applyMiddleware(thunk)))
+const store = configureStore({reducer : reducers} , compose(applyMiddleware(thunk)))
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
