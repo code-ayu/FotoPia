72d9414c1b8d3338dcb73fb96115395461ce0f5e
ayush <ayushmehrotra1234@gmail.com>
2023-11-09 23:36:35 +0530

    main file fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,19 +4,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import {createStore , applyMiddleware,compose } from 'redux';
+import { applyMiddleware,compose } from 'redux';
+import {configureStore} from '@reduxjs/toolkit'
 import thunk from 'redux-thunk';
 import reducers from './reducers'
 import './index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const store = createStore(reducers , compose(applyMiddleware(thunk)))
+
+const store = configureStore(reducers , compose(applyMiddleware(thunk)))
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
     <App />
     </Provider>
-  
   </React.StrictMode>,
 )
  
